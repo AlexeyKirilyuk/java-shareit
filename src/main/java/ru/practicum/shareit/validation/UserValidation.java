e03ba9485a3b60eef15a8dc652d359fc0341bb38
AlexeyKirilyuk <AlexeyKirilyuk@yandex.ru>
2023-06-20 23:59:53 +0500

    java-shareit 1.4

--- src/main/java/ru/practicum/shareit/validation/UserValidation.java
+++ src/main/java/ru/practicum/shareit/validation/UserValidation.java
@@ -26,8 +26,8 @@ public class UserValidation {
         } else if (user.getName() == null || Objects.equals(user.getName(), "")) {
             user.setName(user.getEmail());
         } else for (Map.Entry<Integer, User> entry : users.entrySet()) {
-            User SavedUser = entry.getValue();
-            if (SavedUser.getEmail().equals(user.getEmail())) {
+            User savedUser = entry.getValue();
+            if (savedUser.getEmail().equals(user.getEmail())) {
                 log.debug("Ошибка - пользователь с таким Email уже существует.");
                 throw new ConflictException("Ошибка - пользователь с таким Email уже существует.");
             }
@@ -37,20 +37,6 @@ public class UserValidation {
     }
 
     public boolean userUpdateValidation(User user, HashMap<Integer, User> users) {
-    /*    if (user.getEmail() == null) {
-            log.debug("Ошибка валидации - электронная почта не может быть пустой");
-            throw new ValidationException("Ошибка валидации - электронная почта не может быть пустой");
-        } else if (user.getEmail().isEmpty()) {
-            log.debug("Ошибка валидации - электронная почта не может быть пустой");
-            throw new ValidationException("Ошибка валидации - электронная почта не может быть пустой");
-        } else if (!user.getEmail().contains("@")) {
-            log.debug("Ошибка валидации - электронная почта должна содержать символ @");
-            throw new ValidationException("Ошибка валидации - электронная почта должна содержать символ @");
-        } else
-
-        if (user.getName() == null || Objects.equals(user.getName(), "")) {
-            user.setName(user.getEmail());
-        }else   */
         HashMap<Integer, User> userss = new HashMap<>(users);
         userss.remove(user.getId());
         for (Map.Entry<Integer, User> entry : userss.entrySet()) {
