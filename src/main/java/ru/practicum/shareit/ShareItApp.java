e06a64d071cab7b290cf3daa2b82ac1c716970eb
AlexeyKirilyuk <AlexeyKirilyuk@yandex.ru>
2023-08-10 21:50:37 +0500

    java-shareit 3.2

--- src/main/java/ru/practicum/shareit/ShareItApp.java
+++ src/main/java/ru/practicum/shareit/ShareItApp.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * пароль pgAdmin будет iamroot
+  * пароль pgAdmin будет iamroot
 */
 @Slf4j
 @SpringBootApplication
